42688cd2a2d83cd6b9730c7f7d8ff75c2b81a79e
Sergey Romanov <xxsmotur@gmail.com>
2015-06-23 22:14:12 +0500

    update jaccardSim

--- gominhash.go
+++ gominhash.go
@@ -4,6 +4,8 @@ import
 	"hash/adler32"
 )
 
+//Now, in case with one hash function
+
 //Minhash now only for two sets
 func MinHash(set1, set2 [] string) {
 	//sign1 := findMin(set1)
@@ -27,15 +29,19 @@ func findMin(set1 []string)uint32{
 	return min
 }
 
-func jaccardSim(count int, set1, set2 []string) float64 {
-	total := totalElements(set1, set2)
+func strToHash(set1 []string)[]uint32 {
+	return []uint32{1}
+}
+
+func jaccardSim(count int, set1, set2 []uint32) float64 {
+	//total := totalElements(set1, set2)
 	inter := 0
-	for i := 0; i < count; i++ {
+	for i := 0; i < len(set1); i++ {
 		if set1[i] == set2[i] {
 			inter += 1
 		}
 	}
-	return float64(inter)/float64(len(total))
+	return float64(inter)/float64(len(set1))
 
 }
 
